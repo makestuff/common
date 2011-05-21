137e979027ccbbcd9602b6360228bdc9897a54a0
Chris McClelland <github.com@m3.ath.cx>
2011-05-21 11:58:35 +0100

    Construct a very minimal MinGW environment for Windows builds

---
+++ build-msys.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+mkdir -p all
+cd all/
+wget 'http://prdownloads.sourceforge.net/mingw/make-3.81-3-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/tar-1.23-1-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/gzip-1.3.12-2-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/grep-2.5.4-2-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/findutils-4.4.2-2-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/diffutils-2.8.7.20071206cvs-3-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/coreutils-5.97-3-msys-1.0.13-ext.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/coreutils-5.97-3-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/msysCORE-1.0.17-1-msys-1.0.17-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/termcap-0.20050421_1-2-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/bzip2-1.0.5-2-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/bash-3.1.17-4-msys-1.0.16-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/rxvt-2.7.2-3-msys-1.0.14-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/wget-1.12-1-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/patch-2.6.1-1-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/unzip-6.0-1-msys-1.0.13-bin.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/libiconv-1.13.1-2-msys-1.0.13-dll-2.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/libintl-0.17-2-msys-dll-8.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/libregex-1.20090805-2-msys-1.0.13-dll-1.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/libtermcap-0.20050421_1-2-msys-1.0.13-dll-0.tar.lzma?download'
+wget 'http://prdownloads.sourceforge.net/mingw/libopenssl-1.0.0-1-msys-1.0.13-dll-100.tar.lzma?download'
+rm -rf unpack ../msys
+mkdir -p unpack
+cd unpack/
+for i in ../*.tar.lzma*; do tar --lzma -xf $i; done
+cd ..
+mkdir -p ../msys
+mkdir -p ../msys/bin
+mkdir -p ../msys/etc
+cat > ../msys/etc/profile <<EOF
+# For Visual Studio Express 2010:
+export MSVC=10.0
+export MSWSDK=v7.0A
+# For Visual Studio Express 2008:
+#export MSVC=9.0
+#export MSWSDK=v6.0A
+
+export INCLUDE="C:/Program Files/Microsoft Visual Studio \$MSVC/VC/INCLUDE;C:/Program Files/Microsoft SDKs/Windows/\$MSWSDK/include"
+export LIB="C:/Program Files/Microsoft Visual Studio \$MSVC/VC/LIB;C:/Program Files/Microsoft SDKs/Windows/\$MSWSDK/lib"
+export PATH="/bin:/c/Program Files/Microsoft Visual Studio \$MSVC/Common7/IDE:/c/Program Files/Microsoft SDKs/Windows/\$MSWSDK/bin:/c/Program Files/Microsoft Visual Studio \$MSVC/VC/bin:.:\$PATH"
+export PS1="\${USERNAME}@\${HOSTNAME}\$ "
+EOF
+cp unpack/bin/bunzip2.exe ../msys/bin/
+cp unpack/bin/bzip2.exe ../msys/bin/
+cp unpack/bin/cat.exe ../msys/bin/
+cp unpack/bin/cmp.exe ../msys/bin/
+cp unpack/bin/cp.exe ../msys/bin/
+cp unpack/bin/dd.exe ../msys/bin/
+cp unpack/bin/diff.exe ../msys/bin/
+cp unpack/bin/dirname.exe ../msys/bin/
+cp unpack/bin/find.exe ../msys/bin/
+cp unpack/bin/grep.exe ../msys/bin/
+cp unpack/bin/gunzip ../msys/bin/
+cp unpack/bin/gzip.exe ../msys/bin/
+cp unpack/bin/ls.exe ../msys/bin/
+cp unpack/bin/make.exe ../msys/bin/
+cp unpack/bin/mkdir.exe ../msys/bin/
+cp unpack/bin/msys-1.0.dll ../msys/bin/
+cp unpack/bin/msys-crypto-1.0.0.dll ../msys/bin/
+cp unpack/bin/msys-iconv-2.dll ../msys/bin/
+cp unpack/bin/msys-intl-8.dll ../msys/bin/
+cp unpack/bin/msys-regex-1.dll ../msys/bin/
+cp unpack/bin/msys-ssl-1.0.0.dll ../msys/bin/
+cp unpack/bin/msys-termcap-0.dll ../msys/bin/
+cp unpack/bin/msys-W11.dll ../msys/bin/
+cp unpack/bin/mv.exe ../msys/bin/
+cp unpack/bin/patch.exe ../msys/bin/
+cp unpack/bin/pwd.exe ../msys/bin/
+cp unpack/bin/rm.exe ../msys/bin/
+cp unpack/bin/rxvt.exe ../msys/bin/
+cp unpack/bin/sh.exe ../msys/bin/
+cp unpack/bin/tail.exe ../msys/bin/
+cp unpack/bin/tar.exe ../msys/bin/
+cp unpack/bin/tr.exe ../msys/bin/
+cp unpack/bin/uname.exe ../msys/bin/
+cp unpack/bin/unzip.exe ../msys/bin/
+cp unpack/bin/wget.exe ../msys/bin/
+cp unpack/etc/inputrc.default ../msys/etc/
+cp unpack/etc/termcap ../msys/etc/
+cp unpack/etc/wgetrc ../msys/etc/
+cd ..
+rm -rf all
+cp -rp build-msys.sh msys/
+#zip -r msys.zip msys
+#rm -rf msys
