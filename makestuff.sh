39042686551b0cd0fd767301183cd53ea786ed76
Chris McClelland <github.com@m3.ath.cx>
2011-05-22 01:27:13 +0100

    Script to make build environments

---
+++ makestuff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export DATE=$(date +%Y%m%d)
+cd ${HOME}
+mkdir makestuff
+cd makestuff
+wget -O common.tar.gz --no-check-certificate https://github.com/makestuff/common/tarball/master
+tar zxf common.tar.gz 
+mv makestuff-common-* common
+rm -f common.tar.gz 
+cd common/
+./build-msys.sh 
+mv msys ..
+cd ..
+mkdir libs
+mkdir apps
+cd ..
+zip -r makestuff-win32-${DATE}.zip makestuff
+mv makestuff-win32-${DATE}.zip /mnt/ukfsn/bin/
+rm -rf makestuff/msys
+tar zcf makestuff-linux-${DATE}.tar,gz makestuff
+mv makestuff-linux-${DATE}.tar.gz /mnt/ukfsn/bin/
+rm -rf makestuff
