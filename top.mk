dcc2faa65fe4b1722163db01b3e430aad76c80d1
makestuff <github.com@m3.ath.cx>
2013-05-26 17:04:12 +0100

    Change i686 to x86 and x86_64 to x64 properly

--- top.mk
+++ top.mk
@@ -42,11 +42,14 @@ else
 			PLATFORM := lin
 		endif
 		ifeq ($(MACHINE),x86_64)
-			ARCHFLAGS := -m64 -DBYTE_ORDER=1234
 			MACHINE := x64
 		else ifeq ($(MACHINE),i686)
-			ARCHFLAGS := -m32 -DBYTE_ORDER=1234
 			MACHINE := x86
+		endif
+		ifeq ($(MACHINE),x64)
+			ARCHFLAGS := -m64 -DBYTE_ORDER=1234
+		else ifeq ($(MACHINE),x86)
+			ARCHFLAGS := -m32 -DBYTE_ORDER=1234
 		else ifneq (,$(findstring armv,$(MACHINE)))
 			ABI := $(shell gcc -dumpmachine)
 			ifeq ($(ABI),arm-linux-gnueabihf)
